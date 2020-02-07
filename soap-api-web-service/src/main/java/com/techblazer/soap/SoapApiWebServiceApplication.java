de80bd02387313656e7f83b54c32dfaf10649e0d
promise <promiseokosun@gmail.com>
2020-02-07 16:43:19 +0100

    soap api update

---
+++ soap-api-web-service/src/main/java/com/techblazer/soap/SoapApiWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.techblazer.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapApiWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapApiWebServiceApplication.class, args);
+	}
+
+}
---
+++ soap-api-web-service/src/test/java/com/techblazer/soap/SoapApiWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techblazer.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapApiWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
